33ff2f2d654b5a9be8b8676a2b08cdf674dcccab
vladk <vladk@devart.com>
2020-03-05 13:22:31 +0200

    13,22

---
+++ Tables/dbo.table1.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[table1] (
+  [column1] [datetime] NOT NULL,
+  CONSTRAINT [PK_table1_col] PRIMARY KEY CLUSTERED ([column1])
+)
+ON [PRIMARY]
+GO
+
+SET QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+CREATE TRIGGER [dbo].[trigger1]
+ON [dbo].[table1]
+AFTER INSERT
+AS
+SELECT 1;
+GO
